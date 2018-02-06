a2a6edc932232c69720af8bc81b7407c089116e4
Soki Wada <sokinin1103@yahoo.co.jp>
2018-02-06 15:22:59 +0900

    fix bag

--- lib/random_check_question/8.rb
+++ lib/random_check_question/8.rb
@@ -2,5 +2,5 @@ numbers = [1, 2, 3, 4, 5, 6]
 even_numbers = numbers.select { |n| n.even? }
 even_numbers
 
-non_multiple_of_three = numbers.reject{ |n| n % 3 == 0 }
+non_multiple_of_three = numbers.reject { |n| n % 3 == 0 }
 non_multiple_of_three
--- workshop/question.rb
+++ workshop/question.rb
@@ -1,6 +1,10 @@
-numbers = [1, 2, 3, 4, 5, 6]
-even_numbers = numbers.select { |n| n.even? }
-even_numbers
+person = {
+  name: 'Alice'
+  age: 20,
+  friends: ['Bob', 'Carol']
+  phones: { home: '1234-0000', mobile: '5678-0000' }
+}
 
-non_multiple_of_three = numbers.reject{ |n| n % 3 == 0 }
-non_multiple_of_three
+person[:age]
+person[:friends]
+person[:phones][:mobile]
