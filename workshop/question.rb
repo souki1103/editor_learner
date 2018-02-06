373783391b115415ad713b639a649b3e5b033e1c
Soki Wada <sokinin1103@yahoo.co.jp>
2018-02-06 13:05:43 +0900

    docs

--- workshop/question.rb
+++ workshop/question.rb
@@ -1,7 +1,5 @@
-currencies = { 'japan' => 'yen', 'us' => 'dollar', 'india' => 'rupee' }
-
-currencies.each do |key_value|
-  key = key_value[0]
-  value = key_value[1]
-  puts "#{key} : #{value}"
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n|
+  sum += n
 end
