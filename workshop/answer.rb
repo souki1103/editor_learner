2c65985d94ac8bb46becfc53f01aacab40eb5fa6
Soki Wada <sokinin1103@yahoo.co.jp>
2018-02-15 14:07:13 +0900

    change handout

--- workshop/answer.rb
+++ workshop/answer.rb
@@ -1,7 +1,6 @@
-def foo(time = Time.now, message = bar)
-  puts "time: #{time}, message: #{message}"
-end
-
-def bar
-  'BAR'
+# coding: utf-8
+if '123-4567' =~ /\d{3}-\d{4}/
+  puts 'マッチしました'
+else
+  puts 'マッチしませんでした'
 end
